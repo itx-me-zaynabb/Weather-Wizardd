791c9d0dbeb49e5b757da043cb5a11dca0fadc00
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-03-19 22:15:47 +0500

    import Theme from themetoggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import WeatherPage from "./pages/WeatherPage";
+import ThemeToggle from "./components/Themetoggle";
 
 function App() {
   return (
     <BrowserRouter>
+      <ThemeToggle />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/weather/:city" element={<WeatherPage />} />

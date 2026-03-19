5b3904158b3de423619d71fb6c36e341143fa917
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-03-19 22:00:25 +0500

    WeatherPage

---
+++ src/pages/WeatherPage.jsx
@@ -0,0 +1,56 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getWeather, getForecast } from "../services/weatherApi";
+import { motion } from "framer-motion";
+
+export default function WeatherPage() {
+  const { city } = useParams();
+
+  const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    Promise.all([getWeather(city), getForecast(city)])
+      .then(([w, f]) => {
+        setWeather(w);
+        setForecast(f.list.slice(0, 5));
+      })
+      .finally(() => setLoading(false));
+  }, [city]);
+
+  if (loading)
+    return <p className="text-white text-center mt-10">Loading...</p>;
+
+  if (!weather) return <p className="text-white text-center mt-10">No Data</p>;
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-700 text-white p-6">
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="bg-white/10 backdrop-blur-xl p-6 rounded-3xl max-w-md mx-auto"
+      >
+        <h1 className="text-3xl font-bold">{weather.name}</h1>
+        <p className="text-5xl">{Math.round(weather.main.temp)}°C</p>
+
+        <div className="mt-4 grid grid-cols-2 gap-2">
+          <div>💧 {weather.main.humidity}%</div>
+          <div>🌬 {weather.wind.speed} m/s</div>
+        </div>
+      </motion.div>
+
+      {/* Forecast */}
+      <div className="flex gap-4 mt-6 overflow-x-auto">
+        {forecast.map((f, i) => (
+          <div key={i} className="bg-white/10 p-4 rounded-xl min-w-[120px]">
+            <p>{new Date(f.dt_txt).getHours()}:00</p>
+            <p>{Math.round(f.main.temp)}°C</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}

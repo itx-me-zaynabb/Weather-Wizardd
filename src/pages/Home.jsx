ae29b412950c4cd863994fb52f8b0f7ef9ec3cc7
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-03-19 22:31:23 +0500

    Weather Wizard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center bg-black text-white">
       <div className="w-[300px]">
+        <h1 className="text-4xl font-bold text-center mb-6 animate-pulse">
+          🌤 Weather Wizard 🌈
+        </h1>
         <SearchBar />
       </div>
     </div>

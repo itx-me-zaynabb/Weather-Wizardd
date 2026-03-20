6601a60afcc2323a5725ecb3efc110d51555673c
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-03-20 05:56:42 +0500

    structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import WeatherPage from "./pages/WeatherPage";
 import Weather from "./components/Weather";
-import ThemeToggle from "./components/Themetoggle";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <ThemeToggle />
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route path="/weather/:city" element={<WeatherPage />} />
-        </Routes>
-      </BrowserRouter>
       <Weather />
     </>
   );
